4f67cbbda2419f53bd144a65d15ff5c6c7733053
KsenNeko <70853791+KsenNeko@users.noreply.github.com>
2021-02-18 13:11:37 +0400

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -8,6 +8,9 @@ module.exports = function dateSample(/* sampleActivity */) {
     if (isNan(a)) {
         return false;
     }
+    if (a<=0 || a>= MODERN_ACTIVITY){
+        return false;
+    }
     let k = 0.693 / HALF_LIFE_PERIOD;
     return Math.floor(Math.log(parseFloat(sampleActivity)/MODERN_ACTIVITY)/k)*-1;
 };
