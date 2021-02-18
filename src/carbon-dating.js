d9e0ac855ded170a514ce97d6b46af6f3f8aaf0c
KsenNeko <70853791+KsenNeko@users.noreply.github.com>
2021-02-18 13:07:28 +0400

    feat: add function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,6 +4,10 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+   let a = Number(sampleActivity);
+    if (isNan(a)) {
+        return false;
+    }
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    return Math.floor(Math.log(parseFloat(sampleActivity)/MODERN_ACTIVITY)/k)*-1;
 };
