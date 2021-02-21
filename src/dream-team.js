db6476e3ca4ef4d215732aaf7f9f75b18a34976c
KsenNeko <70853791+KsenNeko@users.noreply.github.com>
2021-02-21 17:23:13 +0400

    fix: removing spaces

--- src/dream-team.js
+++ src/dream-team.js
@@ -4,14 +4,12 @@ module.exports = function createDreamTeam(members) {
   if(!Array.isArray(members)) return false;
     res = '';
     for(human in members) {
-        members[human] = members[human].replace(/\s+/g, '');
-    }
-    members.sort();
-    for(human in members) {
         if(typeof human === 'string'){
+        members[human] = members[human].replace(/\s+/g, '');
         res = res + members[human][0];
         }
     }
     if (res === '')  return false;
+    res.split("").sort().join("");
     return res.toUpperCase();
 };
