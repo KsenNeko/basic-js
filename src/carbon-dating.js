742b78b9b62c9c880135ec5a3d651ce465c06f5e
KsenNeko <70853791+KsenNeko@users.noreply.github.com>
2021-02-21 15:47:44 +0400

    fix: function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,11 +4,8 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
-   let a = Number(sampleActivity);
-    if (isNaN(a)) {
-        return false;
-    }
-    if (a<=0 || a>= MODERN_ACTIVITY){
+    let a = Number(sampleActivity);
+    if(typeof sampleActivity !== "string" || a<=0 || a >= MODERN_ACTIVITY || isNaN(a)) {
         return false;
     }
     let k = 0.693 / HALF_LIFE_PERIOD;
