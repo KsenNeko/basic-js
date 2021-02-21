9bdd2a7566bf6e1d34c313c478e608b0864b4a1c
KsenNeko <70853791+KsenNeko@users.noreply.github.com>
2021-02-21 17:33:12 +0400

    fix: sort

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,15 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  if(!Array.isArray(members)) return false;
+   if(!Array.isArray(members)) return false;
     res = '';
     for(human in members) {
-        if(typeof human === 'string' && members[human] !== ''){
+        if(typeof members[human] === 'string'){
         members[human] = members[human].replace(/\s+/g, '');
         res = res + members[human][0];
         }
     }
     if (res === '')  return false;
-    res.split("").sort().join("");
-    return res.toUpperCase();
+    res = res.split('').sort().join('').toUpperCase();
+    return res;
 };
