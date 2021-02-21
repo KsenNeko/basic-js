692737361c2633d29ebe3de0133a1d85d6899804
KsenNeko <70853791+KsenNeko@users.noreply.github.com>
2021-02-21 17:03:37 +0400

    feat: function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(typeof members !== 'array') return false;
+    res = '';
+    for(human in members) {
+        members[human] = members[human].replace(/\s+/g, '');
+    }
+    members.sort();
+    for(human in members) {
+        if(typeof human === 'string'){
+        res = res + members[human][0];
+        }
+    }
+    if (res === '')  return false;
+    return res.toUpperCase();
 };
