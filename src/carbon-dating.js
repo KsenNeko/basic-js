5c338c44d44c1a4cd464ca5914141f6c426b43b5
KsenNeko <70853791+KsenNeko@users.noreply.github.com>
2021-02-21 15:54:53 +0400

    fix: function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,11 +3,12 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
+module.exports = function dateSample(sampleActivity) {
     let a = Number(sampleActivity);
     if(typeof sampleActivity !== "string" || a<=0 || a >= MODERN_ACTIVITY || isNaN(a)) {
         return false;
     }
     let k = 0.693 / HALF_LIFE_PERIOD;
     return Math.floor(Math.log(parseFloat(sampleActivity)/MODERN_ACTIVITY)/k)*-1;
-};
+  };
+
