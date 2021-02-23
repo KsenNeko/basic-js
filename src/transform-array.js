1c47aec7cdea72a0598611f084472d0aad149223
KsenNeko <70853791+KsenNeko@users.noreply.github.com>
2021-02-23 16:05:06 +0400

    feat: function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw Error;
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "string") {
+      switch (arr[i]) {
+        case "--discard-next":
+          i++;
+          break;
+        case "--discard-prev":
+          if (i > 0 && res[res.length - 1]===arr[i-1]){
+            res.splice(res.length - 1, 1);
+          }
+          break;
+        case "--double-next":
+          if (i < arr.length - 1) {
+            res.push(arr[i + 1]);
+          }
+          break;
+        case "--double-prev":
+          if (i > 0) {
+            res.push(arr[i - 1]);
+          }
+          break;
+      }
+    } else {
+      res.push(arr[i]);
+    }
+  }
+  return res;
+}
