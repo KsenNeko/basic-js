6f1177783b6ff794c274699d183ea75034c055ff
KsenNeko <70853791+KsenNeko@users.noreply.github.com>
2021-03-03 16:57:04 +0400

    feat: function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,50 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  flag = true;
+ constructor(a = true){
+    this.flag = a;
   }
-}
+    encrypt(message, key) {
+      while(key.length < message.length)
+      {
+          key = key+key;
+      }
+      message = message.toUpperCase();
+      key = key.toUpperCase();
+      let res = [];
+      for(let i=0, j=0; i<message.length; i++)
+      {
+        if(/[a-z]/i.test(message[i]))
+        {
+          res[i] = String.fromCharCode((message.charCodeAt(i) +  key.charCodeAt(j))%26 + 65);
+          j++;
+        } else  res[i] = message[i];
+      }
+      if(this.flag)
+      return res.join('');
+      return res.reverse().join('');
+    }    
+    decrypt(encryptedMessage, key) {
+      while(key.length < encryptedMessage.length)
+      {
+          key = key+key;
+      }
+      encryptedMessage = encryptedMessage.toUpperCase();
+      key = key.toUpperCase();
+      let res = [];
+      for(let i=0, j=0; i<encryptedMessage.length; i++)
+      {
+        if(/[a-z]/i.test(encryptedMessage[i]))
+        {
+          res[i] = String.fromCharCode((encryptedMessage.charCodeAt(i) -  key.charCodeAt(j)+26)%26 + 65);
+          j++;
+        } else  res[i] = encryptedMessage[i];
+      }
+      if(this.flag)
+      return res.join('');
+      return res.reverse().join('');
+    }
+  }
 
 module.exports = VigenereCipheringMachine;
